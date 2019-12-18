e77bbbcbc9129d967acdb5060983bb574c027209
leo-holanda <leo.uldholanda@gmail.com>
2019-12-18 18:13:35 -0300

    add transfer class

---
+++ src/com/leonardo/transfer/Transfer.java
@@ -0,0 +1,38 @@
+package com.leonardo.transfer;
+
+import com.leonardo.person.Person;
+import com.leonardo.player.Player;
+import com.leonardo.staff.Staff;
+
+import java.util.ArrayList;
+
+public class Transfer {
+    ArrayList<Person> transferList = new ArrayList<Person>();
+
+    public void add(Person person){
+        transferList.add(person);
+    }
+
+    public void showAll(){
+        Person person;
+        for (int i = 0; i < transferList.size(); i++){
+            person = transferList.get(i);
+
+            if(person instanceof Player){
+                System.out.println("ID: " + i);
+                ((Player) person).showPlayer();
+            }
+            else if(person instanceof Staff){
+                System.out.println("ID: " + i);
+                ((Staff) person).showStaff();
+            }
+            else{
+                System.out.println("Erro! Tente novamente");
+            }
+        }
+    }
+
+    public void delete(int id){
+        transferList.remove(id);
+    }
+}
